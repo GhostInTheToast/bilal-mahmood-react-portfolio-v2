dda1c0d086ef1b403ad71174b1fa227d7e64f876
Bilal Mahmood <bmood1@gmail.com>
2025-02-07 13:52:31 -0600

    prg save Avatar fixed

--- src/about/subheadingsData.js
+++ src/about/subheadingsData.js
@@ -18,23 +18,37 @@ const subheadingsData = {
   ],
   2: [
     {
-      title: "2016 Masters Degree",
+      title: "2025 Artificial Intelligence & Machine Learning PGP",
       content: (
         <>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            In 2025, I completed a post graduate program from The University of Texas at Austin. I now hold a certificate
+            for Artificial Intelligence & Machine Learning: Business Applications The University of Texas, Austin - McCombs
+            School of Business. 
           </p>
         </>
       ),
     },
     {
-      title: "2014 Bachelors Degree",
+      title: "2022 Masters Degree",
       content: (
         <>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            In 2022, I graduated from Lamar University with a Master's Degree in Computer Science.
+            I graduated rank 1 with a 4.00 GPA while doing a thesis route. My thesis has been presented and talked about
+            at the annual HCI Conference in Denmark too by others.
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2020 Bachelors Degree",
+      content: (
+        <>
+          <p>
+            I got my Bachelors of Science in Computer Science in 2020 from Lamar University.
+            My multiple President's and Dean's List accomplishments netted me a scholarship to pursue
+            my Masters Degree afterwards.
           </p>
         </>
       ),
