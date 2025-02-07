40695415636b12837edc1977d6476d340f37647c
Bilal Mahmood <bmood1@gmail.com>
2025-02-07 00:30:03 -0600

    prg save

---
+++ src/about/subheadingsData.js
@@ -0,0 +1,80 @@
+const subheadingsData = {
+  1: [
+    {
+      title: "About Me",
+      content: (
+        <>
+          <p>
+            Hello! I am Full-Stack Software Engineer with over 3 years of experience!
+            My main specialty and experience is in working with and creating REST API's
+            and Microservices in the Java Springboot world. 
+            </p>
+            <p>Additionally, my frontend is decent
+            too as you can see by this portfolio site where i used ReactJS!
+          </p>
+        </>
+      ),
+    },
+  ],
+  2: [
+    {
+      title: "2016 Masters Degree",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2014 Bachelors Degree",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </>
+      ),
+    },
+  ],
+  3: [
+    {
+      title: "2022 Front End Developer",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2020 UX Designer",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2017 IT Consultant",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </>
+      ),
+    },
+  ],
+};
+
+export default subheadingsData;
